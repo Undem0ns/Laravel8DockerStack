828d7498e4066609e4cf03b437d2f285a2b1b9c9
Undem0ns <andamannira@kkumail.com>
2022-08-09 16:40:52 +0700

    add login logout register api

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,10 @@ use App\Http\Controllers\ProductController;
 //     return $request->user();
 // });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::resource('products', ProductController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
